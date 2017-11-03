f7691cd2007adfca3a283d9238dbd7f5e8345abe
Jenna Ellis <ellis93@purdue.edu>
2017-11-03 02:36:36 -0400

    created meeting tests

---
+++ tests/meetings.tests.js
@@ -0,0 +1,48 @@
+const axios  = require('axios')
+    , qs     = require('qs')
+    , chai   = require('chai')
+    , ObjectId = require('mongodb').ObjectId
+    , chaiAsPromised = require('chai-as-promised')
+
+const connectToDatabase = require('../index').connectToDatabase
+    , url = require('../index').ENDPOINT
+
+chai.use(chaiAsPromised)
+
+var assert = chai.assert
+    , expect = chai.expect
+    , should = chai.should()
+
+var db = undefined
+
+var createMeeting = function(emails, meetingName){
+    return new Promise((resolve, reject)=> {
+
+    })
+}
+
+var searchMeeting = function(query){
+    return new Promise((resolve, reject)=> {
+
+    })
+}
+
+Describe('Meeting Creation Tests', function() {
+    before(function(done) {
+        connectToDatabase()
+        .then((result)=>{
+            assert.notEqual(null, result)
+            db = result
+        })
+        .then(() => {
+            return createMeeting('ellis93@purdue.edu pkfire@gmail.com', 'Test Meeting')
+        })
+        .then(() => {
+            return searchMeeting('Test')
+        })
+        .then(() => done())
+        .catch((err)=>{
+            if(err) console.log(err)
+        })
+    })
+})
